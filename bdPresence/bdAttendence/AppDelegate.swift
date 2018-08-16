870bf891cb663f5be62d8e4aae660cc70e3deabc
sssuourabh <saurabhp9@gmail.com>
2018-08-16 17:11:10 +0530

    Add all changes

--- bdPresence/bdAttendence/AppDelegate.swift
+++ bdPresence/bdAttendence/AppDelegate.swift
@@ -194,11 +194,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                             if completionStatus == "Success" {
                                 UserDefaults.standard.set(Date(), forKey: UserDefaultsKeys.LastAssignmentFetched.rawValue)
                             }
-                            print(AssignmentModel.statusOfUser())
+                            //print(AssignmentModel.statusOfUser())
                             
                         }
                     } else {
-                        print(AssignmentModel.statusOfUser())
+                        //print(AssignmentModel.statusOfUser())
                         
                     }
                 }else {
@@ -208,7 +208,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                         if completionStatus == "Success" {
                             UserDefaults.standard.set(Date(), forKey: UserDefaultsKeys.LastAssignmentFetched.rawValue)
                         }
-                        print(AssignmentModel.statusOfUser())
+                        //print(AssignmentModel.statusOfUser())
                     }
                 }
             }
@@ -619,25 +619,25 @@ extension AppDelegate:UNUserNotificationCenterDelegate{
     }
 }
 
-//extension UIApplication{
-//    var topViewController: UIViewController?{
-//        if keyWindow?.rootViewController == nil{
-//            return keyWindow?.rootViewController
-//        }
-//
-//        var pointedViewController = keyWindow?.rootViewController
-//
-//        while  pointedViewController?.presentedViewController != nil {
-//            switch pointedViewController?.presentedViewController {
-//            case let navagationController as UINavigationController:
-//                pointedViewController = navagationController.viewControllers.last
-//            case let tabBarController as UITabBarController:
-//                pointedViewController = tabBarController.selectedViewController
-//            default:
-//                pointedViewController = pointedViewController?.presentedViewController
-//            }
-//        }
-//        return pointedViewController
-//
-//    }
-//}
+extension UIApplication{
+    var topViewController: UIViewController?{
+        if keyWindow?.rootViewController == nil{
+            return keyWindow?.rootViewController
+        }
+        
+        var pointedViewController = keyWindow?.rootViewController
+        
+        while  pointedViewController?.presentedViewController != nil {
+            switch pointedViewController?.presentedViewController {
+            case let navagationController as UINavigationController:
+                pointedViewController = navagationController.viewControllers.last
+            case let tabBarController as UITabBarController:
+                pointedViewController = tabBarController.selectedViewController
+            default:
+                pointedViewController = pointedViewController?.presentedViewController
+            }
+        }
+        return pointedViewController
+        
+    }
+}
